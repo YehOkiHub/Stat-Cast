7543f28bb09f92b00a87671457bcf6fc4ca26728
YehOkiHub <jeff077872@hotmail.com>
2023-02-11 23:05:53 -0500

    did some basic css of the navbar

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,9 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Nav() {
+
     return(
-        <div>
-            Nav
+        <div class="container">
+        <nav className="nav">
+          <ul>
+            <li>
+              <Link className="navLink" to={"/"}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link className="navLink" to={"/Stats"}>
+                Stats
+              </Link>
+            </li>
+            <li>
+              <Link className="navLink" to={"/Teams"}>
+                Teams
+              </Link>
+            </li>
+            <li>
+              <Link className="navLink" to={"/Shop"}>
+                Shop
+              </Link>
+            </li>
+          </ul>
+        </nav>
         </div>
     )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -14,7 +14,7 @@ const routes = createBrowserRouter([
         element: <Home/>
     }, 
     {
-        path: "/stats",
+        path: "/Stats",
         element: <Stats/>
 
     },
