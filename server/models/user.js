20212910f60245c995226dad16ac4ab4b22c5367
YehOkiHub <jeff077872@hotmail.com>
2023-02-19 21:56:48 -0500

    added login, registration components as well as made authentication through graphql

--- server/models/user.js
+++ server/models/user.js
@@ -1,10 +1,7 @@
 const {Schema, model} = require("mongoose");
 
 const userSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
+    
     username: {
         type: String,
         required: true,
--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -3,9 +3,11 @@ const statsRoutes = require('./statRoutes');
 const teamRoutes = require('./teamRoutes');
 const shopRoutes = require('./shopRoutes');
 
+
 router.use('/stats', statsRoutes);
 router.use('/team', teamRoutes);
 router.use('/shop', shopRoutes);
+
 
 module.exports = router;
 
